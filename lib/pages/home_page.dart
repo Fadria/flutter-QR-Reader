1b2b03024dda568f32a086a65c09b68ea033a0a3
Federico Adria <fadriacarrasco@gmail.com>
2021-09-24 10:30:52 +0200

    Diseño de la aplicación

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qrreader/widgets/custom_navigatorbar.dart';
+import 'package:qrreader/widgets/scan_button.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -6,9 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: new AppBar
+      (
+        elevation: 0,
+        title: Text('Historial'),
+        actions: 
+        [
+          IconButton(onPressed: (){}, icon: Icon(Icons.delete_forever))
+        ],
+      ),
       body: Center(
         child: Text('Home Page'),
      ),
+     bottomNavigationBar: CustomNavigationBar(),
+     floatingActionButton: ScanButton(),
+     floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
    );
   }
 }
