732989f6bd54e48c52c9d412b6fdd338a2f96457
Federico Adria <fadriacarrasco@gmail.com>
2021-10-04 08:27:02 +0200

    Cambiar tipo de mapa y punto central

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -19,17 +19,19 @@ class _MapaPageState extends State<MapaPage>
   @override
   Widget build(BuildContext context) 
   {
-    final CameraPosition puntoInicial = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962),
-      zoom: 14.4746,
-    );
-
     final ScanModel scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
+
+    final CameraPosition puntoInicial = CameraPosition(
+      target: scan.getLatLng(),
+      zoom: 17.5,
+    );
 
     return Scaffold(
       appBar: AppBar(title: Text('Mapa')),
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        mapType: MapType.normal,
         initialCameraPosition: puntoInicial,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
